e55c86344c6cd669f924c6994291d7896d8cd8ce
Pro-official <promise.iam00@gmail.com>
2022-02-24 15:22:04 +0600

    Done

--- src/components/PhoneIndividual/PhoneIndividual.jsx
+++ src/components/PhoneIndividual/PhoneIndividual.jsx
@@ -13,13 +13,13 @@ const PhoneIndividual = () => {
         return (
                 <Col key={_id}>
                     <Card className='text-decoration-none phone'>
-                        <Link to={`/phones/${_id}`}><Card.Img className='phone-image ' variant="top" src={image} /></Link>
+                        <Link to={`/phones/${_id}`}><Card.Img className='phone-image ' variant="top" src={image} alt={title}/></Link>
                             <Card.Body>
                                 <div className='d-flex justify-content-between align-items-center'>
                                 <Card.Title className='fw-bolder phone-name'>{title}</Card.Title>
                                 </div>
                                 <Card.Text className='phone-desc'>
-                                    {description.slice(0, 120)}... <Link className='text-decoration-none' to={`/phones/${_id}`}>View Details...</Link>
+                                    {description.slice(0, 120)}... <Link className='text-decoration-none text-primary' to={`/phones/${_id}`}>View Details...</Link>
                                 </Card.Text>
                             </Card.Body>
                     </Card>
